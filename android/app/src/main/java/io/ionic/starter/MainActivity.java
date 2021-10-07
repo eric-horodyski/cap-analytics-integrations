176d512f0970f65e1e4eed1173c07e4658e4cdfe
Eric Horodyski <horodyski@ionic.io>
2021-10-07 17:26:57 -0400

    feat(android): integrate New Relic

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,15 @@
 package io.ionic.starter;
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.BridgeActivity;
+import com.newrelic.agent.android.NewRelic;
+
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    NewRelic.withApplicationToken("AAf5387ca815ba6e368277b375b180ec1bc693bfdb-NRMA").start(this.getApplicationContext());
+  }
+}
