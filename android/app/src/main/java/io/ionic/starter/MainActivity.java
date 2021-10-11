7a47da95ffe6fb285fa0908023ab99a92a70516c
Eric Horodyski <horodyski@ionic.io>
2021-10-11 13:54:33 -0400

    feat(android): integrate Firebase Analytics

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -3,6 +3,7 @@ package io.ionic.starter;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.community.firebaseanalytics.FirebaseAnalytics;
 import com.newrelic.agent.android.NewRelic;
 
 public class MainActivity extends BridgeActivity {
@@ -10,6 +11,7 @@ public class MainActivity extends BridgeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    registerPlugin(FirebaseAnalytics.class);
     NewRelic.withApplicationToken("AAf5387ca815ba6e368277b375b180ec1bc693bfdb-NRMA").start(this.getApplicationContext());
   }
 }
